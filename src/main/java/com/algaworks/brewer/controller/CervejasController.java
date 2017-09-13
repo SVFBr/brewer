0473a30cf77508615efc755e5b9babc26e06d6aa
root <sheilafreire.br@gmail.com>
2017-09-12 21:29:33 -0300

    38 Foward e Redirect

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -18,13 +20,17 @@ public class CervejasController {
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 
-		System.out.println(result.hasErrors() ? " Campo vazio " : "");
-
-		System.out.println(">>>>> log.console [CervejasController] : CadastroCerveja.html : [" + cerveja.getSku() + "]["
-				+ cerveja.getNome() + "]");
-		return "cerveja/CadastroCerveja";
+		if (result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário");
+			System.out.println("LOG : Validação NOK");
+			return "cerveja/CadastroCerveja";
+		} else {
+			attributes.addFlashAttribute("mensagem", "Erro no formulário");
+			System.out.println(">>>>> LOG " + cerveja.getSku() + " " + cerveja.getNome());
+			return "redirect:/cervejas/novo";
+		}
 
 	}
 
